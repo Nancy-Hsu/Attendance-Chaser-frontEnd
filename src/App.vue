4d315f26559f0be15a84f161645d07eeb9a3130f
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-12-29 14:22:30 +0800

    feat: add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
-<script>
+<script setup>
   import "bootstrap/dist/css/bootstrap.min.css"
   import "bootstrap"
+  import "@/assets/CSS/bootstrap.min.css"
+  import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
+  <Navbar />
   <router-view v-slot="{Component}">
     <component :is="Component" :key="$route.path"></component>
   </router-view>
-
 </template>
 
 <style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-info px-4">
+    <div class="container-fluid ">
+      <router-link class="navbar-brand fs-4" to="/">
+        Titaners' 出勤系統
+      </router-link>
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
+        aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" id="navbarColor01" style="">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+          <button type="button" class="btn btn-sm btn-outline-light my-2 my-sm-0">
+            登出
+          </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
